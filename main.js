f3616ab11af1e4f17e1d17af32d9adf0d9182431
Deep <115367789+deepuh@users.noreply.github.com>
2023-01-14 12:26:50 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+﻿const game = new GameEngine();
+
+const ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./ship.png");
+
+ASSET_MANAGER.downloadAll(() => {
+    const canvas = document.getElementById("gameWorld");
+    const ctx = canvas.getContext("2d");
+
+    game.addEntity(new Ship(game));
+
+    game.init(ctx);
+
+    game.start();
+});
